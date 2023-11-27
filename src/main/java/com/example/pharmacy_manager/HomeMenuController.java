f84e6323c5d0b394530d5c2706cf51eb57571c3c
man250001 <monizcr@oldcolony.info>
2023-11-27 10:20:23 -0500

    File changes

--- src/main/java/com/example/pharmacy_manager/HomeMenuController.java
+++ src/main/java/com/example/pharmacy_manager/HomeMenuController.java
@@ -3,7 +3,7 @@ package com.example.pharmacy_manager;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class HomeMenuController {
     @FXML
     private Label welcomeText;
 
