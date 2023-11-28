9694b33a0d92aee7d064a06e3cd4f35c919d85c7
man250001 <monizcr@oldcolony.info>
2023-11-28 11:30:34 -0500

    Handle logging in from users

--- src/main/java/com/example/pharmacy_manager/LoginController.java
+++ src/main/java/com/example/pharmacy_manager/LoginController.java
@@ -1,8 +1,35 @@
 package com.example.pharmacy_manager;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private TextField usernameField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        loginButton.setOnAction(event -> {
+            try {
+                if (DBUtils.logInUser(event, usernameField.getText(), passwordField.getText())) {
+                    DBUtils.changeScene(event, "homeMenu.fxml", "Home Menu");
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
 }
