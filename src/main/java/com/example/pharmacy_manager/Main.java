0e67c650ddf07761793f6930f5e199defbb0bf51
man250001 <monizcr@oldcolony.info>
2023-11-28 12:03:32 -0500

    Fixed main

--- src/main/java/com/example/pharmacy_manager/Main.java
+++ src/main/java/com/example/pharmacy_manager/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HomeMenu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
