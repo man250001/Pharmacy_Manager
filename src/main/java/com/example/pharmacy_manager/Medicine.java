72e72eb035f0f578bb7c3e1ee5bfdd557ab86e6a
man250001 <monizcr@oldcolony.info>
2023-12-07 11:12:39 -0500

    Fix Warning: Remove unneeded suppression

--- src/main/java/com/example/pharmacy_manager/Medicine.java
+++ src/main/java/com/example/pharmacy_manager/Medicine.java
@@ -1,6 +1,5 @@
 package com.example.pharmacy_manager;
 
-@SuppressWarnings("unused")
 public record Medicine(int id, int medicineId, String brand, String productName, String type, String status,
                        double price, String date) {
 }
